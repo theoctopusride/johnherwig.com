5d95697a1830c07bdf8e3b15b9000613cfac0857
theoctopusride <herwig.6@gmail.com>
2020-01-14 15:10:48 -0500

    committed original website ideas

---
+++ app/config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+  port: process.env.PORT || 8080,
+  db: 'mongodb://localhost/myblog',
+  test_port: 4242,
+  test_db: 'mongodb://localhost/myblog_test'
+}module.exports = config;
